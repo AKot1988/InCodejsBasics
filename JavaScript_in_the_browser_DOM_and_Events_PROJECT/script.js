c4c8e033986d3ff2293f36d05540c57cef6f55e0
AKot <@kulibabenko@ukr.net>
2026-01-07 15:56:27 +0200

    DOM Project

---
+++ JavaScript_in_the_browser_DOM_and_Events_PROJECT/script.js
@@ -0,0 +1,78 @@
+'use strict';
+
+// ----------поверне будь яке число з діапазону між заданими
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// ----------міняє мекткс месседжу у просимспному ДОМ елементі
+function displayedMessage(message, element) {
+  element.textContent = `${message}`;
+}
+
+// varriables to owerride
+let startNumberInterval = 1;
+let finishNumberInterval = 20;
+let secretNumber = randomInt(startNumberInterval, finishNumberInterval);
+let score = finishNumberInterval;
+let highScore = 0;
+const randomNumber = randomInt(startNumberInterval, finishNumberInterval); //number
+
+const messageOption = {
+  initial: 'Start guessing...',
+  low: '📉 Too low!',
+  high: '📈 Too high!',
+  correct: '🎉 Correct Number!',
+  noNumber: '⛔️ No number!',
+  lose: 'you lose',
+};
+
+// get DOM elements to interaction
+const body = document.querySelector('body');
+const messageElement = document.querySelector('.message');
+const againButton = document.querySelector('.again');
+const checkButton = document.querySelector('.check');
+const scoreIndication = document.querySelector('.score');
+const hiScoreIndication = document.querySelector('.highscore');
+const inputNumber = document.querySelector('.guess');
+
+checkButton.addEventListener('click', () => {
+  let numberSugestion = Number(inputNumber.value);
+  if (!numberSugestion) {
+    displayedMessage(messageOption.noNumber, messageElement);
+  } else if (score <= 1) {
+    displayedMessage(messageOption.lose, messageElement);
+    checkButton.disabled = true;
+    checkButton.style.color = 'transparent';
+  } else if (score > 0) {
+    if (numberSugestion < randomNumber) {
+      score--;
+      displayedMessage(messageOption.low, messageElement);
+      displayedMessage(score, scoreIndication);
+    } else if (numberSugestion > randomNumber) {
+      score--;
+      displayedMessage(messageOption.high, messageElement);
+      displayedMessage(score, scoreIndication);
+    } else if (numberSugestion === randomNumber) {
+      displayedMessage(messageOption.correct, messageElement);
+      highScore < score
+        ? ((highScore = score), displayedMessage(highScore, hiScoreIndication))
+        : null;
+      //next we have oport. to push one more class name to elem, or extract one. But...
+      body.style.backgroundColor = 'teal';
+      checkButton.disabled = true;
+      checkButton.style.color = 'transparent';
+    }
+  }
+});
+
+againButton.addEventListener('click', () => {
+  score = finishNumberInterval;
+  randomInt(startNumberInterval, finishNumberInterval);
+  displayedMessage(messageOption.initial, messageElement);
+  displayedMessage(score, scoreIndication);
+  body.style.backgroundColor = '#222';
+  checkButton.disabled = false;
+  checkButton.style.color = '#222';
+  inputNumber.value = '';
+});
