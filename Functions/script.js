07e31cceccd9a0714ca938d432a3fbcd66993324
AKot <@kulibabenko@ukr.net>
2026-01-16 13:54:11 +0200

    functionChallenge 1

---
+++ Functions/script.js
@@ -0,0 +1,96 @@
+// // we have an next example(based on closure):
+
+// const greet = function (greetingMessage) {
+//   return function (name) {
+//     console.log(`${greetingMessage} ${name}`);
+//   };
+// };
+
+// const greetHallo = greet('Hello');
+
+// greetHallo('Anton');
+// greet('Pryvit')('Kysia');
+// ////////////////////////////////////////////////////////////////////////////////////////////
+
+// // via arrow function
+// const pereVirkaDokymentiv = (vitannia) => (vmistPytannya) =>
+//   console.log(`${vitannia}  ${vmistPytannya}`);
+// pereVirkaDokymentiv('Anton, dobrogo vechora,')('perevirka dokymentiv');
+
+// //////////////////////////bind//////////////////////////
+// const someObjWithMethod = {
+//   dealerName: 'AClima llc',
+//   soldItems: [],
+//   addButton: undefined,
+//   addItem: function (stuff, quantity) {
+//     console.log(this);
+//     this.soldItems.push([stuff, quantity]);
+//     console.log(this.soldItems);
+//     console.log(this);
+//     const inputElem = document.querySelector('.itemName');
+//     const addButton = document.querySelector('.submit');
+//     addButton.textContent = this.dealerName;
+
+//     addButton.addEventListener('click', () => {
+//       this.soldItems.push([inputElem.value, quantity]);
+//       console.log(this.dealerName, this.soldItems);
+//     });
+//   },
+// };
+
+// // someObjWithMethod.addItem('zakatochna mashynka', 15);
+// const nextObjectAsysLLC = {
+//   dealerName: 'ASYSLLC',
+//   soldItems: [],
+// };
+// const addItems = someObjWithMethod.addItem;
+// const addItemsASYSLLC = addItems.bind(nextObjectAsysLLC);
+// addItemsASYSLLC(14, 'Kosilka');
+
+// ---------------------------------------CHALEENGE#1------------------------------------------------
+// const answersString =
+//   'What is your favorite programming language?\n0: JavaScript\n1: Pyton\n3: Rust\n4: C++\n(Write option number)';
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😄
+  generatePromptString: function (question, options) {
+    let optionString = this.question;
+    let updatedOptions = '';
+    for (const [index, option] of this.options.entries()) {
+      updatedStr = `${index + 1}: ${option.slice(3)}`;
+      optionString += `\n${updatedStr}`;
+    }
+    return optionString;
+  },
+  answers: new Array(4).fill(0),
+  registerNewAnswer: function () {
+    const getInput = prompt(this.generatePromptString());
+    (Number.isInteger(getInput) && getInput < 0) || getInput > 3
+      ? (alert('Please type number equal of your answer'),
+        this.registerNewAnswer())
+      : (this.answers[+`${getInput - 1}`]++, this.displayResults());
+  },
+
+  displayResults: function () {
+    console.log(this.answers);
+  },
+};
+
+const poolButton = document.querySelector('.poll');
+
+poolButton.addEventListener('click', () => poll.registerNewAnswer());
+
+// ------------------------------------------------bonus data-------------------------------------------------------
+const bonusTestData1 = [5, 2, 3];
+let testObject1 = {};
+testObject1.answers = bonusTestData1;
+testObject1.displayResults = poll.displayResults.bind(testObject1);
+testObject1.displayResults();
+
+const bonusTestData2 = [1, 5, 3, 9, 6, 1];
+let testObject2 = {};
+testObject2.answers = bonusTestData2;
+testObject2.displayResults = poll.displayResults.bind(testObject2);
+testObject2.displayResults();
+// testObject2.
