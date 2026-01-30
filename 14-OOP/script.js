40e0af3c5851815bdf05a547ec6b845df615382b
AKot <@kulibabenko@ukr.net>
2026-01-30 10:00:59 +0200

    Third commitd chalenge OOP Done

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,4 +1,6 @@
 'use strict';
+
+// --------------------------------------First challenge--------------------------------------
 const Car = function (brand, speed) {
   this.brand = brand;
   this.speed = speed;
@@ -17,31 +19,31 @@ Car.prototype.brake = function () {
 const bmw = new Car('BMW', 120);
 bmw.accelerate();
 bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
-bmw.brake();
 
 const mercedes = new Car('Mercedes', 95);
 mercedes.accelerate();
+
+// --------------------------------------Second challenge--------------------------
+class CarCl {
+  constructor(brand, speed) {
+    this.brand = brand;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.brand} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.brand} is going at ${this.speed} km/h`);
+    }
+    
+    get speedUS() { 
+    return this.speed / 1.6;
+  }
+    set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
