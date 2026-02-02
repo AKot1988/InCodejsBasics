f2fb31f6f1c294ccc09970e1a83810b44b5a099f
AKot <@kulibabenko@ukr.net>
2026-02-02 11:44:15 +0200

    Mapty 02_02_02commit section 15

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -1,4 +1,5 @@
 'use strict';
+import { map } from './mapLeaf.js';
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -9,4 +10,3 @@ const inputType = document.querySelector('.form__input--type');
 const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
