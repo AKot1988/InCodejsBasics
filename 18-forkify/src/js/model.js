31fdf0c41425d022a128b5a12f211990b18fe098
AKot <kulibabenko@ukr.net>
2026-02-19 16:28:50 +0200

    bookmark feat"

--- 18-forkify/src/js/model.js
+++ 18-forkify/src/js/model.js
@@ -2,6 +2,7 @@ import { async } from 'regenerator-runtime';
 import { API_URL } from './config.js';
 import { GET_JSON } from './helpers.js';
 import { PAGE, RES_PER_PAGE } from './config.js';
+
 export const state = {
   recipe: {},
   recipyParentElement: null,
@@ -11,6 +12,7 @@ export const state = {
     resultsPerPage: RES_PER_PAGE,
     currentPage: PAGE,
   },
+  bookmarks: [],
 };
 
 export const loadRecipe = async function (id) {
@@ -27,6 +29,10 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
+
+    if (state.bookmarks.some(bookmark => bookmark.id === id))
+      state.recipe.bookmarked = true;
+    else state.recipe.bookmarked = false;
   } catch (err) {
     console.error(`${err} 💥💥💥`);
     throw err;
@@ -62,6 +68,17 @@ export const updateServings = function (newServings) {
     ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
     state.recipe.servings = newServings;
   });
+};
+
+export const addBookmark = function (recipe) {
+  state.bookmarks.push(recipe);
+  if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+};
+
+export const deleteBookmark = function (id) {
+  const index = state.bookmarks.findIndex(el => el.id === id);
+  state.bookmarks.splice(index, 1);
+  if (id === state.recipe.id) state.recipe.bookmarked = false;
 };
 
 // loadSearchResults('pasta');
--- 18-forkify/src/js/views/recipeView.js
+++ 18-forkify/src/js/views/recipeView.js
@@ -22,6 +22,13 @@ export class RecipeView extends View {
       // if (updateTo > 0) handler(updateTo);
     });
   }
+  addHandlerBookmark(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const button = e.target.closest('.btn--bookmark');
+      if (!button) return;
+      handler();
+    });
+  }
 
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
@@ -69,9 +76,9 @@ export class RecipeView extends View {
               <use href="${icons}#icon-user"></use>
             </svg>
           </div>
-          <button class="btn--round">
+          <button class="btn--round btn--bookmark">
             <svg class="">
-              <use href="${icons}#icon-bookmark-fill"></use>
+              <use href="${icons}#icon-bookmark${this._data.bookmarked ? '-fill' : ''}"></use>
             </svg>
           </button>
         </div>
